4e5114c0073b57710e2758e7dd88add20b777a8d
Mats Gustafsson <mats@mcnab.se>
2018-03-06 09:35:53 +0100

    makes side bar conditional

--- apperance.el
+++ apperance.el
@@ -3,8 +3,12 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq inhibit-startup-message t) ;; hide the startup message
 ;; enable line numbers globally and add a padding of choice
-(global-linum-mode t) 
-(setq linum-format "%4d \u2502 ") ; vertical bar
+(global-linum-mode t)
+(if (display-graphic-p)
+    (progn
+    ;; if graphic
+      ())
+      (setq linum-format "%4d \u2502 ")) ; vertical bar
 ;;(setq linum-format "%d ") ; just a space
 
 ;; Window size and start position
@@ -20,14 +24,4 @@
        (load-theme 'leuven t))
     ;; else (on a terminal, seems ok on termius terminal set to "vt100")
        (load-theme 'whiteboard))
-;(load-theme 'tango-plus)
-;(load-theme 'whiteboard)
-;(load-theme 'flatui))
-;(load-theme 'twilight-bright t)
-;(load-theme 'gandalf t)
-;(load-theme 'tango)
-;(load-theme 'adwaita)
-;(load-theme 'leuven t)
-;(load-theme 'github-modern t)
-
 
