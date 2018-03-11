fc197e4d2e5c0df309b92b39f34688ec5b31a931
Mats Gustafsson <hem@mats-gustafsson.se>
2018-03-11 07:34:32 +0100

    removes some files from git

--- org-clock-save.el
+++
@@ -1,2 +0,0 @@
-;; org-persist.el - raspberrypi at <2018-03-10 Sat 07:16>
-(setq stored-clock-history '(("/home/mats/gitdev/planning/cellscan-tabor-wcdma.org" . 235) ("/home/mats/gitdev/planning/cellscan-tabor-wcdma.org" . 420)))
