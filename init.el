9f7878b9ed101af62c466501fa6fb2165b5dd0e6
mats <mats@mcnab.se>
2018-03-20 08:04:20 +0100

    upds

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(add-to-list 'load-path "~/.emacs.d/custom")
+(add-to-list 'load-path "~/.emacs.d/cpp_custom")
 
 (require 'setup-general)
 (if (version< emacs-version "24.4")
