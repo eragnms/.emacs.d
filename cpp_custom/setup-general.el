9b1be357924db6f97c4f921fb0f3afc2c2a10d20
Mats Gustafsson <hem@mats-gustafsson.se>
2018-04-07 22:33:49 +0200

    removes gdb many windows as default mode

--- cpp_custom/setup-general.el
+++ cpp_custom/setup-general.el
@@ -26,7 +26,7 @@
 ;; setup GDB
 (setq
  ;; use gdb-many-windows by default
- gdb-many-windows t
+ ;;gdb-many-windows t
 
  ;; Non-nil means display source file containing the main routine at startup
  gdb-show-main t
