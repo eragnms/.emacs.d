6ae7a94efdd70de804bcf3e40b1d8d4bb24619b6
mats <mats@mcnab.se>
2018-09-27 10:01:50 +0200

    remove double setting of org agenda weekday start

--- init.el
+++ init.el
@@ -46,7 +46,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-start-on-weekday 1)
  '(package-selected-packages
    (quote
     (graphql magit magit-p4 org-bullets projectile smartparens smartparens-config srefactor company-c-headers wgrep use-package sr-speedbar py-autopep8 org iedit helm-projectile helm-gtags function-args flycheck elpy better-defaults)))
