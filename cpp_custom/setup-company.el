07738c8ac56190a2add74b88e2ab9639caa5b3ee
mats <mats@mcnab.se>
2018-09-28 08:05:10 +0200

    add code to speed up company and semantics

--- cpp_custom/setup-company.el
+++ cpp_custom/setup-company.el
@@ -11,3 +11,16 @@
 (use-package company-c-headers
   :init
   (add-to-list 'company-backends 'company-c-headers))
+
+;; Supposed to solve an issue with company and semantics being slow
+;; https://github.com/syl20bnr/spacemacs/issues/11058#issuecomment-407269954
+;; https://github.com/syl20bnr/spacemacs/issues/1907
+(defun et/semantic-remove-hooks ()
+    (remove-hook 'completion-at-point-functions
+                 'semantic-analyze-completion-at-point-function)
+    (remove-hook 'completion-at-point-functions
+                 'semantic-analyze-notc-completion-at-point-function)
+    (remove-hook 'completion-at-point-functions
+                 'semantic-analyze-nolongprefix-completion-at-point-function))
+
+  (add-hook 'semantic-mode-hook #'et/semantic-remove-hooks)
