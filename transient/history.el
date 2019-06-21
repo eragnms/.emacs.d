0b2d1a85ae26c4cb539e7bb68d401793715bdfa2
Mats Gustafsson <mats@mcnab.se>
2019-06-21 08:51:49 +0200

    adds transient folder

---
+++ transient/history.el
@@ -0,0 +1,11 @@
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
+ (magit-dispatch nil)
+ (magit-fetch nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate"))
+ (magit-merge nil)
+ (magit-pull nil)
+ (magit-push nil))
